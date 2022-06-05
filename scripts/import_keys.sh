84805f47fbbec3927099e9cfc21822cc2c7fd2b3
Hemanth Bollamreddi <blmhemu@gmail.com>
2022-06-05 18:48:42 +0530

    Updated few params and script

--- scripts/import_keys.sh
+++ scripts/import_keys.sh
@@ -1,8 +1,13 @@
+KEYCHAIN_NAME=homelab
+KEYCHAIN=$KEYCHAIN_NAME.keychain
+# create keychain
+security create-keychain $KEYCHAIN
+security list-keychains -d user -s $(security list-keychains -d user | sed -e s/\"//g) $KEYCHAIN
 # nomad
-security import files/tls/nomad/ca/nomad-agent-ca.pem -k ~/Library/Keychains/Cluster.keychain-db
-security import files/tls/nomad/cli/global-cli-nomad-0.pem -k ~/Library/Keychains/Cluster.keychain-db
-security import files/tls/nomad/cli/global-cli-nomad-0-key.pem -k ~/Library/Keychains/Cluster.keychain-db
+security import files/tls/nomad/ca/nomad-agent-ca.pem -k $KEYCHAIN 
+security import files/tls/nomad/cli/global-cli-nomad-0.pem -k $KEYCHAIN
+security import files/tls/nomad/cli/global-cli-nomad-0-key.pem -k $KEYCHAIN
 # consul
-security import files/tls/consul/ca/consul-agent-ca.pem -k ~/Library/Keychains/Cluster.keychain-db
-security import files/tls/consul/cli/dc1-cli-consul-0.pem -k ~/Library/Keychains/Cluster.keychain-db
-security import files/tls/consul/cli/dc1-cli-consul-0-key.pem -k ~/Library/Keychains/Cluster.keychain-db
+security import files/tls/consul/ca/consul-agent-ca.pem -k $KEYCHAIN
+security import files/tls/consul/cli/dc1-cli-consul-0.pem -k $KEYCHAIN
+security import files/tls/consul/cli/dc1-cli-consul-0-key.pem -k $KEYCHAIN
