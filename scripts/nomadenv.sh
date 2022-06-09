5d1b90e6b2af8b82912f7d8051bc8ea178918ea9
Hemanth Bollamreddi <blmhemu@gmail.com>
2022-06-09 17:47:24 +0530

    Added cni stuff

---
+++ scripts/nomadenv.sh
@@ -0,0 +1,4 @@
+export NOMAD_ADDR=https://192.168.0.100:4646
+export NOMAD_CACERT=files/tls/nomad/ca/nomad-agent-ca.pem
+export NOMAD_CLIENT_CERT=files/tls/nomad/cli/global-cli-nomad-0.pem
+export NOMAD_CLIENT_KEY=files/tls/nomad/cli/global-cli-nomad-0-key.pem
